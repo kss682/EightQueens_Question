52e1a9d03b3c13f898eadc92d4ddcea2a08b6f9d
kss682 <kss682@gmail.com>
2021-07-11 13:32:39 +0530

    Logic added to GameStrategy and ChessBoardRenderer

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,7 +2,14 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
+		boolean isBlack = false;
 		System.out.println(square);
-		return true;
+		if(((square >= 0 && square <= 7) || (square >= 16 && square <= 23) || (square >= 32 && square <= 39) || (square >= 48 && square <= 55)) && square%2 != 0) {
+			isBlack = true;
+		}
+		else if(((square >= 8 && square <= 15) || (square >= 24 && square <= 31) || (square >= 40 && square <= 47) || (square >= 56 && square <= 63)) && square%2 == 0) {
+			isBlack = true;
+		}
+		return isBlack;
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -4,14 +4,12 @@ public class GameStrategy {
 
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		return cellId%8;	// just for the heck of it
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		return cellId/8;	// just for the heck of it
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -29,6 +27,53 @@ public class GameStrategy {
 			WRITE YOUR LOGIC HERE...............................
 
 		*/
+		int temp_r, temp_c;
+		
+		if(placedQueens[row][col] == false) {
+			for(int i = 0; i<8; i++) {
+				placedQueens[row][i] = true;
+				placedQueens[i][col] = true;
+			}
+			
+
+			temp_r = row;
+			temp_c = col;
+			while(temp_r + 1 <= 7 && temp_c + 1 <= 7) {
+				temp_r++;
+				temp_c++;
+				placedQueens[temp_r][temp_c] = true;
+			}
+		
+			temp_r = row;
+			temp_c = col;
+			while(temp_r + 1 <= 7 && temp_c - 1 >= 0) {
+				temp_r++;
+				temp_c--;
+				placedQueens[temp_r][temp_c] = true;
+			}
+			
+			temp_r = row;
+			temp_c = col;
+			while(temp_r - 1 >= 0 && temp_c + 1 <= 7) {
+				temp_r--;
+				temp_c++;
+				placedQueens[temp_r][temp_c] = true;
+			}
+			
+			temp_r = row;
+			temp_c = col;
+			while(temp_r - 1 >= 0 && temp_c - 1 >= 0) {
+				temp_r--;
+				temp_c--;
+				placedQueens[temp_r][temp_c] = true;
+			}
+			isValid = true;
+			numQueens++;
+		}
+		else {
+			isValid = false;
+		}
+		
 		return isValid;
 	}
 }
